4c37c8fa4969bc54dc1dc0ade9d20531ab8ee137
pardige <140949466+Pardigle@users.noreply.github.com>
2025-03-24 13:58:39 +0800

    Edited

--- M5E-DjangoTemplates-ChuaLacsonLuciano/tapas/tapasapp/views.py
+++ M5E-DjangoTemplates-ChuaLacsonLuciano/tapas/tapasapp/views.py
@@ -7,7 +7,6 @@ def view_basic_list(request):
     dish_objects = Dish.objects.all()
     return render(request, 'tapasapp/basic_list.html', {'dishes':dish_objects})
 
-
 def view_menu(request):
     dish_objects = Dish.objects.all()
     return render(request, 'tapasapp/list.html', {'dishes':dish_objects})
